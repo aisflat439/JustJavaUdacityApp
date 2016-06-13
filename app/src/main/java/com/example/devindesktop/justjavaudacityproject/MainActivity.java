d4c272eea7b983194571040a40389e8c8f8cf645
Devin P <devinfitzsimons@yahoo.com>
2016-06-13 13:22:23 -0400

    add buttons for increment and decrement quantity

--- app/src/main/java/com/example/devindesktop/justjavaudacityproject/MainActivity.java
+++ app/src/main/java/com/example/devindesktop/justjavaudacityproject/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,12 +16,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View v){
-        display(1);
+        int quantity = 2;
+        double priceOfCofees = 0;
+        double paperCupCharge = 0;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     private void display(int number){
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
