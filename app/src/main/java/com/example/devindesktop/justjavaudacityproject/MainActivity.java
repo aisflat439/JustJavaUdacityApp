dde427b91e5d0f7589970619ebfe2aa7037860a9
Devin P <devinfitzsimons@yahoo.com>
2016-06-13 12:00:49 -0400

    Began implementing main activity java. Created simple method to update quantity on  button press.

--- app/src/main/java/com/example/devindesktop/justjavaudacityproject/MainActivity.java
+++ app/src/main/java/com/example/devindesktop/justjavaudacityproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.devindesktop.justjavaudacityproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View v){
+        display(1);
+    }
+
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
